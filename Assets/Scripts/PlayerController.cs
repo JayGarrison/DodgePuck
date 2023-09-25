5bd229b962818d1a6ea531f981a28593ad469227
JayGarrison <24garrj@elida.k12.oh.us>
2023-09-25 10:52:07 -0400

    3
    Movements

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,52 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public int score = 7;
+    public float speed = 10.0f;
+    public float xRange;
+    public float yRange;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void LateUpdate()
+    {
+        //Keep Player within xRange (Left and Right sides)
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector2(xRange, transform.position.y);
+        }
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector2(-xRange, transform.position.y);
+        }
+        //Keep Player within yRange (Up and Down sides)
+        if (transform.position.y > yRange)
+        {
+            transform.position = new Vector2(transform.position.x, yRange);
+        }
+
+        if (transform.position.y < -yRange)
+        {
+            transform.position = new Vector2(transform.position.x, -yRange);
+        }
+    }
+    
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            Debug.Log("You pressed D");
-        }
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Debug.Log(moveHorizontal);
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        transform.Translate(movement * speed * Time.deltaTime);
     }
 }
